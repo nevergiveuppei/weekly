45f15aa68915bc55a7299eb68e0dcb75d744f607
nevergiveuppei <100401871+nevergiveuppei@users.noreply.github.com>
2022-02-28 09:30:51 +0800

    upload test

---
+++ 0219.py
@@ -0,0 +1,34 @@
+
+#input=test_string
+#print(test_string)
+#res=len(test_string.split())
+#print("The number of words in string are:"+str(res))
+
+#剪刀石頭布（Rock, Paper, Scissors）
+#顧名思義，就是寫一個能跟你猜拳的程式啦！
+
+import random
+print(random.randrange(0,99))
+
+
+#ll = 0
+#for i in range(1,10,1):
+#    for j in range (1,10,1): 
+#        k=i*j
+#        ll=ll+1
+#        print("%d*%d=%d"%(i,j,k),ll)
+
+#for i in range(0,10,1):
+#    for j in range (3,0,-1): 
+#        print(i*j)
+
+# C language
+#for(i=0; i<10; i++)    {
+#              for(j=33; j>0;j--) {
+#     printf("xxx")
+#              }
+#      }
+
+#sequences = [0,1,2,3,4,5]
+#for i in sequences:
+#    print(i)
